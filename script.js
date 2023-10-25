c31b63a9c9d26d9412713a07987932b5e77c9882
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-10-25 07:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  // console.log(num.toString(num));
+	let bin = 0;
+	let i,rem=1;
+	while(num!=0)
+	{
+		rem = num%2;
+		num = parseInt(num/2);
+		bin = bin + rem*i;
+		i = i*10;
+	}
+	console.log(bin);
 }
 
 window.decimalToBinary = decimalToBinary;
